416c829df41c229c9addf852d4adc7b9828133a6
comcinco <comcinco@gmail.com>
2021-09-02 15:05:02 +0200

    [Salvatore] [source:lang/ plan] Export depuis https://trad.spip.net de la langue es [Salvatore] [source:lang/ plan] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/plan_es.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/plan?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'confirmer_deplacement' => '¿Está seguro de que quiere mover estos artículos?',
+
+	// D
+	'deplacement_en_echec' => 'Fallo al mover.',
+	'deplacement_identifiant_absent' => 'Ninguno ',
+	'deplacement_reussi' => 'Movido con éxito.',
+	'deplacements_en_echec' => '@nb@ desplazamientos fallidos.',
+	'deplacements_reussis' => '@nb@ movimientos exitosos.',
+
+	// E
+	'erreur_aucun_identifiant' => 'No hay identificador de objeto.',
+	'erreur_autorisation_insuffisante' => 'Autorización insuficiente.',
+	'erreur_deplacement' => 'Traslado no realizado.',
+	'erreur_deplacement_impossible' => '¡Imposible de mover!',
+	'erreur_rubriques_parentes_incorrectes' => 'Secciones de origen incorrectas.',
+
+	// P
+	'plan_titre' => 'Mapa del sitio en el área privada',
+
+	// S
+	'suggerer_deplier' => 'Esto puede deberse al movimiento de un grupo grande: en este caso no se carga la lista de sus hijos. Hay que abrir el bloque (se cargará), cerrarlo y volver a moverlo.',
+
+	// T
+	'tous_les_statuts' => 'Todos los estados'
+);
