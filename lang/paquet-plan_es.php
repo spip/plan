b0bafa6b89795ee64c9756780070d4b075746b20
comcinco <comcinco@gmail.com>
2021-09-02 14:55:11 +0200

    [Salvatore] [source:lang/ paquet-plan] Export depuis https://trad.spip.net de la langue es [Salvatore] [source:lang/ paquet-plan] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/paquet-plan_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-plan?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'plan_description' => 'Este plugin mejora la página "Mapa del sitio" del área privada mostrando una estructura desplegable y permitiendo filtrar por estado del artículo.',
+	'plan_nom' => 'Mapa del sitio en el área privada',
+	'plan_slogan' => 'Mejora la página del mapa del sitio del área privada'
+);
