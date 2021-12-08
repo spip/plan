4a4a8dc5aa6fe1ca860a1c92ba674423c71a568f
Matthieu Marcillaud <marcimat@rezo.net>
2021-12-08 17:01:10 +0100

    Report des chaines de langue depuis master (salvatore)

---
+++ lang/paquet-plan_it.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-plan?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'plan_description' => 'Questo plugin migliora la pagina "Mappa del sito" nell’area privata visualizzando una struttura ad albero e consentendo il filtraggio per stato dell’articolo.
+',
+	'plan_nom' => 'Mappa del sito nell’area privata',
+	'plan_slogan' => 'Migliora la mappa del sito nell’area privata'
+);
